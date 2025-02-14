a54b0fbdef60d95a83f296d24682cbc7372a0a4f
AleksiKorhonen06 <aljuko06@gmail.com>
2025-02-14 14:00:23 +0200

    Koodin perusnäkymää ja vähän nappi functioita

--- visual_studio_projekti/Probality_calc/Probality_calc/Probality_calc/Form1.Designer.cs
+++ visual_studio_projekti/Probality_calc/Probality_calc/Probality_calc/Form1.Designer.cs
@@ -28,13 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.D6 = new System.Windows.Forms.Button();
+            this.Coin = new System.Windows.Forms.Button();
+            this.D21 = new System.Windows.Forms.Button();
+            this.D12 = new System.Windows.Forms.Button();
+            this.D40 = new System.Windows.Forms.Button();
+            this.D60 = new System.Windows.Forms.Button();
+            this.AnsInfo = new System.Windows.Forms.Label();
+            this.EventBoard = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // D6
+            // 
+            this.D6.Location = new System.Drawing.Point(93, 7);
+            this.D6.Name = "D6";
+            this.D6.Size = new System.Drawing.Size(75, 59);
+            this.D6.TabIndex = 0;
+            this.D6.Text = "D6";
+            this.D6.UseVisualStyleBackColor = true;
+            this.D6.Click += new System.EventHandler(this.D6_Click);
+            // 
+            // Coin
+            // 
+            this.Coin.Location = new System.Drawing.Point(12, 7);
+            this.Coin.Name = "Coin";
+            this.Coin.Size = new System.Drawing.Size(75, 59);
+            this.Coin.TabIndex = 1;
+            this.Coin.Text = "Coin";
+            this.Coin.UseVisualStyleBackColor = true;
+            this.Coin.Click += new System.EventHandler(this.Coin_Click);
+            // 
+            // D21
+            // 
+            this.D21.Location = new System.Drawing.Point(93, 72);
+            this.D21.Name = "D21";
+            this.D21.Size = new System.Drawing.Size(75, 59);
+            this.D21.TabIndex = 2;
+            this.D21.Text = "D21";
+            this.D21.UseVisualStyleBackColor = true;
+            this.D21.Click += new System.EventHandler(this.D21_Click);
+            // 
+            // D12
+            // 
+            this.D12.Location = new System.Drawing.Point(12, 72);
+            this.D12.Name = "D12";
+            this.D12.Size = new System.Drawing.Size(75, 59);
+            this.D12.TabIndex = 3;
+            this.D12.Text = "D12";
+            this.D12.UseVisualStyleBackColor = true;
+            this.D12.Click += new System.EventHandler(this.D12_Click);
+            // 
+            // D40
+            // 
+            this.D40.Location = new System.Drawing.Point(12, 137);
+            this.D40.Name = "D40";
+            this.D40.Size = new System.Drawing.Size(75, 59);
+            this.D40.TabIndex = 4;
+            this.D40.Text = "D40";
+            this.D40.UseVisualStyleBackColor = true;
+            this.D40.Click += new System.EventHandler(this.D40_Click);
+            // 
+            // D60
+            // 
+            this.D60.Location = new System.Drawing.Point(93, 137);
+            this.D60.Name = "D60";
+            this.D60.Size = new System.Drawing.Size(75, 59);
+            this.D60.TabIndex = 5;
+            this.D60.Text = "D60";
+            this.D60.UseVisualStyleBackColor = true;
+            this.D60.Click += new System.EventHandler(this.D60_Click);
+            // 
+            // AnsInfo
+            // 
+            this.AnsInfo.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.AnsInfo.Location = new System.Drawing.Point(220, 281);
+            this.AnsInfo.Name = "AnsInfo";
+            this.AnsInfo.Size = new System.Drawing.Size(564, 145);
+            this.AnsInfo.TabIndex = 6;
+            // 
+            // EventBoard
+            // 
+            this.EventBoard.AutoScroll = true;
+            this.EventBoard.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.EventBoard.Location = new System.Drawing.Point(223, 12);
+            this.EventBoard.Name = "EventBoard";
+            this.EventBoard.Size = new System.Drawing.Size(404, 220);
+            this.EventBoard.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.D21);
+            this.panel2.Controls.Add(this.D6);
+            this.panel2.Controls.Add(this.D60);
+            this.panel2.Controls.Add(this.Coin);
+            this.panel2.Controls.Add(this.D40);
+            this.panel2.Controls.Add(this.D12);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(181, 256);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button6);
+            this.panel3.Location = new System.Drawing.Point(12, 281);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(181, 145);
+            this.panel3.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 59);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 59);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "custom";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 72);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 59);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel4.Controls.Add(this.checkBox5);
+            this.panel4.Controls.Add(this.checkBox4);
+            this.panel4.Controls.Add(this.checkBox3);
+            this.panel4.Controls.Add(this.checkBox2);
+            this.panel4.Controls.Add(this.checkBox1);
+            this.panel4.Location = new System.Drawing.Point(649, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(135, 256);
+            this.panel4.TabIndex = 8;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(30, 112);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(30, 89);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(30, 66);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(30, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(30, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(223, 238);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 30);
+            this.Clear.TabIndex = 6;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(314, 238);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 30);
+            this.Calculate.TabIndex = 7;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(405, 238);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 30);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.EventBoard);
+            this.Controls.Add(this.AnsInfo);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button D6;
+        private System.Windows.Forms.Button Coin;
+        private System.Windows.Forms.Button D21;
+        private System.Windows.Forms.Button D12;
+        private System.Windows.Forms.Button D40;
+        private System.Windows.Forms.Button D60;
+        private System.Windows.Forms.Label AnsInfo;
+        private System.Windows.Forms.Panel EventBoard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- visual_studio_projekti/Probality_calc/Probality_calc/Probality_calc/Form1.cs
+++ visual_studio_projekti/Probality_calc/Probality_calc/Probality_calc/Form1.cs
@@ -1,20 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
 
 namespace Probality_calc
 {
     public partial class Form1 : Form
     {
+        private int panelOffsetX = 10; // This will track the starting position for the next panel horizontally
+        private int panelOffsetY = 10; // Vertical offset for the next row
+        private const int spaceBetweenPanels = 10; // Space between panels horizontally and vertically
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Attach event handler to buttons
+            Coin.Click += AddInfoBox_Click;
+            D6.Click += AddInfoBox_Click;
+            D12.Click += AddInfoBox_Click;
+            D21.Click += AddInfoBox_Click;
+            D40.Click += AddInfoBox_Click;
+            D60.Click += AddInfoBox_Click;
+            Clear.Click += Clear_Click;
+        }
+        private void Coin_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void D6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void D12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void D21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void D40_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void D60_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddInfoBox_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton == null) return;
+
+            // Create a new Info Box (Panel)
+            Panel infoBox = new Panel
+            {
+                Size = new Size(150, 50),
+                BackColor = Color.LightGray,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            // Create a Label inside the Panel
+            Label infoLabel = new Label
+            {
+                Text = $"Event: {clickedButton.Name}",
+                AutoSize = true,
+                Location = new Point(10, 10)
+            };
+
+            infoBox.Controls.Add(infoLabel);
+
+            // Check if the next panel will exceed the EventBoard width
+            if (panelOffsetX + infoBox.Width > EventBoard.Width)
+            {
+                // Move to the next row if it exceeds the width
+                panelOffsetX = 10; // Reset to the start of the new row
+                panelOffsetY += infoBox.Height + spaceBetweenPanels; // Move down by the height of the panel plus space
+            }
+
+            // Set the panel's location
+            infoBox.Location = new Point(panelOffsetX, panelOffsetY);
+
+            // Add the new panel to the EventBoard
+            EventBoard.Controls.Add(infoBox);
+
+            // Update panelOffsetX for the next panel (move to the right)
+            panelOffsetX += infoBox.Width + spaceBetweenPanels; // Add space between panels horizontally
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            // Create a list to hold panels to be removed
+            List<Control> controlsToRemove = new List<Control>();
+
+            // Add all Panels from EventBoard to the list
+            foreach (Control control in EventBoard.Controls)
+            {
+                if (control is Panel)
+                {
+                    controlsToRemove.Add(control);
+                }
+            }
+
+            // Remove the panels from EventBoard after collecting them in the list
+            foreach (Control control in controlsToRemove)
+            {
+                EventBoard.Controls.Remove(control);
+                control.Dispose(); // Dispose of the panels to free resources
+            }
+
+            // Reset panelOffsetX and panelOffsetY for when new panels are added
+            panelOffsetX = 10;
+            panelOffsetY = 10;
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
